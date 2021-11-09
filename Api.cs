7f2f8a29a32ca0d7d19e7dfd356d767a5c517b27
Abhiram13 <abhiram131869@gmail.com>
2021-11-09 15:50:30 +0530

    commit

--- Api.cs
+++ Api.cs
@@ -6,8 +6,27 @@ using System.Threading.Tasks;
 using System.Threading;
 
 namespace dotnet_websockets {
-   [Route("")]
-   public class Api {
-      
-   }
+	[Route("")]
+	public class Api: Controller {
+		[HttpGet]
+		[Route("soc")]
+		public async Task Sockets() {
+			if (HttpContext.WebSockets.IsWebSocketRequest) {
+				using WebSocket webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+				await Echo(webSocket);
+			} else {
+				HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+			}
+		}
+
+		private async Task Echo(WebSocket webSocket) {
+			var buffer = new byte[1024 * 4];
+			WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+			while (!result.CloseStatus.HasValue) {
+				await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
+				result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+			}
+			await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+		}
+	}
 }
